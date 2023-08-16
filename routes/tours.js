0cdf09131803de5720fc4b45cc22a401886743ce
Nesrine <nesrine.laabidi20@gmail.com>
2023-08-16 12:28:55 +0100

    deploy

---
+++ routes/tours.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import {createTour, deleteTour, getAllTour, getFeaturedTours, getSingleTour, getTourBySearch, getTourCount, updateTour} from './../controllers/tourController.js'
+import { verifyAdmin } from '../utils/verifyToken.js';
+const router = express.Router();
+
+// create new tour 
+router.post('/',verifyAdmin ,createTour);
+// update  tour 
+router.put('/:id', verifyAdmin ,updateTour);
+// delete  tour 
+router.delete('/:id', verifyAdmin ,deleteTour);
+// getSingle  tour 
+router.get('/:id', getSingleTour);
+// getAll  tour 
+router.get('/', getAllTour);
+// get tour by search
+router.get('/search/getTourBySearch', getTourBySearch)
+// get featured tour 
+router.get('/search/getFeaturedTours', getFeaturedTours)
+// get tour counts
+router.get("/search/getTourCount", getTourCount);
+export default router;
